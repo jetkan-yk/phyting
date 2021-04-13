576a761783af423115b4c40d71c4a4ae0d4686a6
Jet Kan <jetkan.yk@gmail.com>
2021-04-13 19:33:55 +0800

    Add path

--- best.py
+++ best.py
@@ -1,3 +1,5 @@
+import copy
+
 def best_itinerary(profit, quarantine, home):
     max_day = len(profit) - 1  # 0-indexed
     max_city = len(profit[0]) - 1  # 0-indexed
@@ -5,17 +7,21 @@ def best_itinerary(profit, quarantine, home):
     def get_idle(city):
         return quarantine[city] if city in range(max_city + 1) else 0
 
-    def earn(day, city, idle):
+    def earn(day, city, idle, path):
         if day > max_day or city < 0 or city > max_city:
             return 0
+        
+        path.append(city)
 
-        stay = earn(day + 1, city, idle - 1) + (profit[day][city] if idle <= 0 else 0)
-        left = earn(day + 1, city - 1, get_idle(city - 1))
-        right = earn(day + 1, city + 1, get_idle(city + 1))
+        stay = earn(day + 1, city, idle - 1, copy.deepcopy(path)) + (profit[day][city] if idle <= 0 else 0)
+        left = earn(day + 1, city - 1, get_idle(city - 1), copy.deepcopy(path))
+        right = earn(day + 1, city + 1, get_idle(city + 1), copy.deepcopy(path))
 
-        return max(stay, left, right)
+        result = max(stay, left, right)
+        print(result, path)
+        return result
 
-    return earn(0, home, 0)
+    return earn(0, home, 0, [])
 
 
 if __name__ == "__main__":
@@ -24,4 +30,4 @@ if __name__ == "__main__":
     quarantine = [3, 1, 5, 1, 2]
     home = 1
 
-    print(best_itinerary(profit, quarantine, home))
+    print(best_itinerary(profit, quarantine, home)) # 111
