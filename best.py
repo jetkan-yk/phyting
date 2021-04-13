8799b6e4a95adeb45e46ecb5aacbcd026c3d3f52
Jet Kan <jetkan.yk@gmail.com>
2021-04-13 19:15:31 +0800

    Add best.py

---
+++ best.py
@@ -0,0 +1,27 @@
+def best_itinerary(profit, quarantine, home):
+    max_day = len(profit) - 1  # 0-indexed
+    max_city = len(profit[0]) - 1  # 0-indexed
+
+    def get_idle(city):
+        return quarantine[city] if city in range(max_city + 1) else 0
+
+    def earn(day, city, idle):
+        if day > max_day or city < 0 or city > max_city:
+            return 0
+
+        stay = earn(day + 1, city, idle - 1) + (profit[day][city] if idle <= 0 else 0)
+        left = earn(day + 1, city - 1, get_idle(city - 1))
+        right = earn(day + 1, city + 1, get_idle(city + 1))
+
+        return max(stay, left, right)
+
+    return earn(0, home, 0)
+
+
+if __name__ == "__main__":
+    profit = [[6, 9, 7, 5, 9], [4, 7, 3, 10, 9], [7, 5, 4, 2, 8],
+              [2, 7, 10, 9, 8], [2, 7, 10, 9, 8], [35, 5, 2, 6, 111]]
+    quarantine = [3, 1, 5, 1, 2]
+    home = 1
+
+    print(best_itinerary(profit, quarantine, home))
