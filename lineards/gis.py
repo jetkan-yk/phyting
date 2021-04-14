a0ff4086500ecb2ba5b0e94e738c2b1f5ecd5e2f
Jet Kan <jetkan.yk@gmail.com>
2021-04-14 17:29:35 +0800

    Complete gis

---
+++ lineards/gis.py
@@ -0,0 +1,15 @@
+# Kattis - greedilyincreasing
+import sys
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, sys.stdin.readline().split()))
+    ans = []
+    
+    for i in range(n):
+        if i == 0 or prev < arr[i]:
+            prev = arr[i]
+            ans.append(prev)
+
+    print(len(ans))
+    print(*ans)
