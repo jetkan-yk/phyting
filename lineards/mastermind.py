74c3f48d11d0c82eda54244117ec2a859f83c348
Jet Kan <jetkan.yk@gmail.com>
2021-04-14 20:30:44 +0800

    Formatting

--- lineards/mastermind.py
+++ lineards/mastermind.py
@@ -8,18 +8,18 @@ if __name__ == "__main__":
     r = s = 0
     code_count = Counter()
     guess_count = Counter()
-    
+
     for i in range(n):
         if code[i] == guess[i]:
             r += 1
         else:
             code_count[code[i]] += 1
             guess_count[guess[i]] += 1
-    
+
     for letter, count in code_count.items():
         if guess_count[letter] == 0:
             continue
         else:
             s += min(count, guess_count[letter])
-        
+
     print(r, s)
