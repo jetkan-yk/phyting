53a30f0840d78786f1cd971f704c60073e923ff0
Jet Kan <jetkan.yk@gmail.com>
2021-04-14 18:13:14 +0800

    Formatting

--- lineards/gis.py
+++ lineards/gis.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     n = int(input())
     arr = list(map(int, sys.stdin.readline().split()))
     ans = []
-    
+
     for i in range(n):
         if i == 0 or prev < arr[i]:
             prev = arr[i]
