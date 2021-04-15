d4113e02f0f5b2447cdeae7bd69acd25e23503a2
Jet Kan <jetkan.yk@gmail.com>
2021-04-15 14:58:48 +0800

    Update baloni solution

--- lineards/baloni.py
+++ lineards/baloni.py
@@ -1,15 +1,15 @@
 # Kattis - baloni
 '''
-    Use of collections.Counter()
+    Initialize large 1D array
 '''
 import sys
-from collections import Counter
 
 if __name__ == "__main__":
+    MAXH = 10**6 + 7
     n = int(input())
     arr = list(map(int, sys.stdin.readline().split()))
     ans = 0
-    arrow = Counter()
+    arrow = [0] * MAXH
 
     for i in arr:
         if arrow[i] == 0: ans += 1
