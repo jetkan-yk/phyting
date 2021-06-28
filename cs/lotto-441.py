af60c230becf09dc0ed81c10e47dc3d4de5cde43
Jet Kan <jetkan-yk@gmail.com>
2021-06-28 20:04:21 +0800

    Complete lotto

--- cs/lotto-441.py
+++ cs/lotto-441.py
@@ -1,9 +1,29 @@
 # UVa - 441 Lotto
 """
-    Iterative Complete Search
-    6 Nested Loops
+    Iterative Complete Search with 6 nested loops
 """
 import sys
+from itertools import combinations
 
 inputs = sys.stdin.read().splitlines()
 outputs = []
+
+for line in inputs:
+    if line == "0":
+        break
+
+    k = int(line.split()[0])
+    S = line.split()[1:]
+
+    for a in range(0, k - 5):
+        for b in range(a + 1, k - 4):
+            for c in range(b + 1, k - 3):
+                for d in range(c + 1, k - 2):
+                    for e in range(d + 1, k - 1):
+                        for f in range(e + 1, k):
+                            outputs.append(
+                                " ".join([S[a], S[b], S[c], S[d], S[e], S[f]])
+                            )
+    outputs.append("")
+
+sys.stdout.write("\n".join(outputs))
