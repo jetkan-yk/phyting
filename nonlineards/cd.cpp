e7f186fb0d9ff7e6afa3fc1f9cc4d4452183eb02
Jet Kan <jetkan.yk@gmail.com>
2021-12-16 20:22:03 +0800

    Add cd

---
+++ nonlineards/cd.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, ans, x;
+unordered_set<int> jack;
+
+int main() {
+    while (cin >> n >> m) {
+        // Keep looping until input is 0 0
+        if (n == 0 && m == 0) break;
+
+        // Reset counters
+        ans = 0;
+        jack.clear();
+
+        // Read input
+        while (n--) {
+            cin >> x;
+            jack.insert(x);
+        }
+        while (m--) {
+            cin >> x;
+            if (jack.find(x) != jack.end()) ans++;
+        }
+
+        // Output answer
+        cout << ans << "\n";
+    }
+}
