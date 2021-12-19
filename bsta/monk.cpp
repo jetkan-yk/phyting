6904574233f844c0ace5914030f745f8eeee9cf8
Jet Kan <jetkan.yk@gmail.com>
2021-12-19 23:11:53 +0800

    Add monk

---
+++ bsta/monk.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAXN 5003
+#define MAXT 5000005
+#define EPS 0.000001
+
+// A segment has a height, time and cumulative time
+struct Segment {
+    int h, t, ct;
+} asc[MAXN], dsc[MAXN];
+
+// Get the height at a specific time, O(n)
+double get_height(const Segment sg[], int n, double time) {
+    // Cumulative height from previous segments
+    double cum_height = 0.0;
+
+    // Find the time's current segment
+    Segment cur;
+    for (int i = 0; i < n; i++) {
+        cur = sg[i];
+        if (time <= sg[i].ct) break;
+        cum_height += sg[i].h;
+    }
+
+    // Relative time within the segment
+    double rel_time = cur.t - (cur.ct - time);
+    // Relative height within the segment
+    double rel_height = (double)cur.h / cur.t * rel_time;
+
+    // Real height at the given time
+    return cum_height + rel_height;
+}
+
+int a, d, total_h;
+
+int main() {
+    // Read input
+    cin >> a >> d;
+    for (int i = 0; i < a; i++) {
+        cin >> asc[i].h >> asc[i].t;
+        asc[i].ct = (i == 0) ? asc[0].t : asc[i - 1].ct + asc[i].t;
+        total_h += asc[i].h;
+    }
+    for (int i = 0; i < d; i++) {
+        cin >> dsc[i].h >> dsc[i].t;
+        dsc[i].ct = (i == 0) ? dsc[0].t : dsc[i - 1].ct + dsc[i].t;
+    }
+
+    // BSTA
+    double lo = 0, hi = MAXT, ans;
+    while (lo <= hi) {
+        double mid = hi - (hi - lo) / 2.0;
+        double asc_height = get_height(asc, a, mid);
+        double dsc_height = total_h - get_height(dsc, d, mid);
+
+        if (fabs(asc_height - dsc_height) < EPS)
+            ans = mid;  // Possible answer
+        if (asc_height < dsc_height)
+            lo = mid + EPS;
+        else
+            hi = mid - EPS;
+    }
+
+    // Output answer
+    printf("%.6f\n", ans);
+}
