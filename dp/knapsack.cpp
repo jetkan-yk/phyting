16dff74e5ee0693d467405dc2a60e05b057c0e01
Jet Kan <jetkan.yk@gmail.com>
2021-12-20 22:31:05 +0800

    Knapsack v1

---
+++ dp/knapsack.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAXN 2003
+#define MAXW 100005
+#define INF 2000000009
+
+int C, n, value[MAXN], weight[MAXN], memo[MAXN][MAXW];
+
+// i is current item, w is the bag's current weight
+int bag(int i, int w) {
+    // Considered all items or bag is full
+    if (i == n || w == C) return 0;
+    // Bag exceeded capacity limit
+    if (w > C) return -INF;
+
+    // Previously computed state
+    if (memo[i][w] != -1) return memo[i][w];
+
+    // Best value if we are taking item i
+    int taking = bag(i + 1, w + weight[i]) + value[i];
+    // Best value if we are not taking item i
+    int not_taking = bag(i + 1, w);
+
+    // If adding item i exceeds bag capacity, we cannot take item i
+    // Otherwise, consider either taking or not taking item i
+    return memo[i][w] = max(taking, not_taking);
+}
+
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    while (cin >> C >> n) {
+        // Read input
+        for (int i = 0; i < n; i++)
+            cin >> value[i] >> weight[i];
+
+        // Reset memo
+        memset(memo, -1, sizeof memo);
+        vector<bool> pick(n, false);
+
+        // Dynamic programming
+        cout << bag(0, 0) << endl;
+
+        // Output answer
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= C; j++)
+                cout << memo[i][j] << " ";
+            cout << endl;
+        }
+    }
+}
