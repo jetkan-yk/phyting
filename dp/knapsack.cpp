8ae74b8cd4704eb5d664ec1e1a35c21078b1adf1
Jet Kan <jetkan.yk@gmail.com>
2021-12-22 18:41:33 +0800

    knapsack topdown

--- dp/knapsack.cpp
+++ dp/knapsack.cpp
@@ -10,7 +10,7 @@ int C, n, value[MAXN], weight[MAXN];
 
 // Find the best value after considering item i with w allowed weight
 int top_down(int i, int w, vvi& memo) {
-    // state is a reference variable, any changes will be reflected on memo
+    // state is a reference variable, any changes to it will be reflected on memo
     int& state = memo[i][w];
 
     // Base case: 0 item or no remaining weight
@@ -19,7 +19,7 @@ int top_down(int i, int w, vvi& memo) {
     // Previously computed state
     if (state != -1) return state;
 
-    // If item is heavier than allowed weight in knapsack, don't take it
+    // If item is heavier than the bag's allowed weight, don't take it
     if (w < weight[i]) return state = top_down(i - 1, w, memo);
     // Otherwise consider either taking or not taking the item
     return state = max(top_down(i - 1, w - weight[i], memo) + value[i],
@@ -27,39 +27,41 @@ int top_down(int i, int w, vvi& memo) {
 }
 
 // Backtrack the memo to trace which items are selected
-void trace(int i, int w, const vvi& memo, vi& item) {
+void trace(int i, int w, const vvi& memo, vi& bag) {
     // Traced all items, job done
     if (i == 0) return;
 
-    if (w < weight[i]) return trace(i - 1, w, memo, item);
-
-    int taking = memo[i - 1][w - weight[i]] + value[i];
-    int not_taking = memo[i - 1][w];
-    if (memo[i][w] == taking) {
-        item.push_back(i);
-        return trace(i - 1, w - weight[i], memo, item);
+    if (memo[i][w] == memo[i - 1][w]) {
+        // Not taking item i
+        return trace(i - 1, w, memo, bag);
     } else {
-        return trace(i - 1, w, memo, item);
+        // Taking item i
+        bag.push_back(i);
+        return trace(i - 1, w - weight[i], memo, bag);
     }
 }
 
 int main() {
     while (cin >> C >> n) {
+        // Fast I/O
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
+
         // Read input
-        for (int i = 1; i <= n; i++)  // 1-indexed
+        for (int i = 1; i <= n; i++)  // Using 1-indexed
             cin >> value[i] >> weight[i];
 
         // Reset memo
         vvi memo = vector<vi>(n + 1, vi(C + 1, -1));
-        vi item = vi();
+        vi bag;
 
         // Dynamic programming
         top_down(n, C, memo);
-        trace(n, C, memo, item);
+        trace(n, C, memo, bag);
 
         // Output answer
-        cout << item.size() << endl;
-        for (int x : item) cout << x - 1 << " ";  // Change to 0-indexed
-        cout << endl;
+        cout << bag.size() << "\n";
+        for (int x : bag) cout << x - 1 << " ";  // Revert to 0-indexed
+        cout << "\n";
     }
 }
