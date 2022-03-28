4efca599afb146cd8f2f3b46fa3cec3f5c779034
Jet Kan <jetkan.yk@gmail.com>
2022-03-28 18:58:47 +0800

    Update pointer lesson

--- misc/pointer.c
+++ misc/pointer.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int a = 10;
+    int a = 1045;
     int* p;
     p = &a;
-
-    printf("Address of p is %d\n", p);
     printf("Size of integer is %d bytes\n", sizeof(int));
-    printf("Address of p is %d\n", p + 2);
-    printf("Content of p + 2 is %d\n", *(p + 2));
+    printf("Address = %d, value = %d\n", p, *p);
+    printf("Address = %d, value = %d\n", p + 1, *(p + 1));
+
+    char *p0;
+    p0 = (char*) p;
+    printf("Size of char is %d bytes\n", sizeof(char));
+    printf("Address = %d, value = %d\n", p0 + 1, *(p0 + 1));
+
+    // Void pointer - generic data type, cannot be dereferenced
+    void *p1;
+    p1 = p0;
+    printf("Address = %d, address + 1 = %d\n", p1, p1 + 1);
 }
